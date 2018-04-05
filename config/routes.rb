f9e0ca6c3acd0056431974cf25dad6155d55c15d
Ethan Streadbeck <estreadbeck@gmail.com>
2018-04-05 11:08:34 -0600

    generated pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  get 'pages/employment'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
